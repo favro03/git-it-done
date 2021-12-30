8922ef2c797d6b49924bc07cfa85ea8dbd68f750
tfavro <wetr9902@gmail.com>
2021-12-29 20:30:01 -0600

    added error handeling and linking to second html pg

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,23 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName){
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else{
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?directions=asc";
@@ -16,7 +34,7 @@ var getRepoIssues = function(repo) {
             });
         }
         else{
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -68,5 +86,5 @@ var displayWarning = function(repo){
     //append to warning container
     limitWarningEl.appendChild(linkEl);
 };
-
-getRepoIssues("expressjs/express");
+getRepoName();
+//getRepoIssues();
